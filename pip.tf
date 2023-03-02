a4b378f721c8e5743b2d35d723320a9613326e4e
Alexander Sharov <kvendingoldo@yandex.ru>
2023-03-02 02:31:42 +0100

    feat: add dependabot

--- pip.tf
+++ pip.tf
@@ -4,6 +4,9 @@ resource "yandex_vpc_address" "pip" {
   }
 
   name = format("%s-nlb-%s", var.name, each.key)
+  description = ""
+  folder_id   = var.folder_id
+  labels      = var.labels
 
   external_ipv4_address {
     zone_id = each.value["external_address_spec"]["pip_zone_id"]
